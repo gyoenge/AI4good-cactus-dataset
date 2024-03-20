0825917e8c99f082a96d6a193f572decc84ed195
ParkJumyung <parkjumyungpublic@gmail.com>
2024-03-20 16:59:16 +0900

    ✨ randomize base visual property

--- ppt_object_synthesizer.py
+++ ppt_object_synthesizer.py
@@ -1,14 +1,14 @@
 from pptx import Presentation
 from pptx.enum.shapes import MSO_SHAPE
-from pptx.util import Inches
-from random import choice
+from pptx.util import Inches, Emu
+import random
 
 class PPT:
     def __init__(self):
         self.prs = Presentation()
 
-        self.prs.slide_width = Inches(4)
-        self.prs.slide_height = Inches(3)
+        self.prs.slide_width = Inches(8)
+        self.prs.slide_height = Inches(6)
 
         self.prs.core_properties.author = "Cactus"
         self.prs.core_properties.category = "Image Dataset"
@@ -18,17 +18,34 @@ class PPT:
         self.prs.core_properties.version = "0.0"
 
         self.shapes = dict((member.name, member.value) for member in MSO_SHAPE.__members__)
+
+        shape_max_size = (Inches(4), Inches(4))
+        self.base_shape_visual_property_range = {
+            "left": (0, self.prs.slide_width.emu - shape_max_size[0].emu), 
+            "top": (0, self.prs.slide_height.emu - shape_max_size[1].emu), 
+            "width": (0, shape_max_size[0].emu), 
+            "height": (0, shape_max_size[1].emu), 
+            "rotation": (0, 360.), 
+        }
     
     def add_blank_slide(self):
         self.prs.slides.add_slide(self.prs.slide_layouts.get_by_name("Blank"))
         
     def generate_random_shape(self):
-        shape_type = choice(list(self.shapes.items()))
+        shape_type = random.choice(list(self.shapes.items()))
         self.prs.slides[-1].shapes.add_shape(
             shape_type[1], 
             Inches(1), Inches(1), Inches(1),  Inches(1)
         )
+
         shape = self.prs.slides[-1].shapes[-1]
+
+        shape.left = Emu(random.uniform(*self.base_shape_visual_property_range["left"]))
+        shape.top = Emu(random.uniform(*self.base_shape_visual_property_range["top"]))
+        shape.width = Emu(random.uniform(*self.base_shape_visual_property_range["width"]))
+        shape.height = Emu(random.uniform(*self.base_shape_visual_property_range["height"]))
+        shape.rotation = random.uniform(*self.base_shape_visual_property_range["rotation"])
+        
         return shape
 
     def save(self):
@@ -37,5 +54,6 @@ class PPT:
 
 ppt = PPT()
 ppt.add_blank_slide()
-ppt.generate_random_shape()
+for i in range(10):
+    ppt.generate_random_shape()
 ppt.save()
