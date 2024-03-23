f82d1a2e70ecab3d1980cb0bdca9d3f17cd4fd8f
ParkJumyung <parkjumyungpublic@gmail.com>
2024-03-23 16:17:32 +0900

    ♻️ ✨ 🎨 generate answer.json, refactor

--- ppt_object_synthesizer.py
+++ ppt_object_synthesizer.py
@@ -2,9 +2,66 @@ from pptx import Presentation
 from pptx.dml.color import RGBColor
 from pptx.enum.shapes import MSO_SHAPE
 from pptx.enum.dml import MSO_LINE
-# from pptx.enum.dml import MSO_FILL
-from pptx.util import Inches
+from pptx.util import Inches, Pt
 import random
+from pptx.shapes.autoshape import Shape
+from pprint import pprint
+import json
+
+
+def k_modal_distribution(modes=[], weights=[]):
+    value = sum([weight * random.gauss(mode) for (mode, weight) in zip(modes, weights)]) / sum(weights)
+    return value if value > 0 else 0
+
+def random_rgb_color():
+    return RGBColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+def add_custom_manipulator(cls:Shape):
+    def set(self, property, value):
+        match property:
+            case "line_type":
+                self.line.dash_style = value
+            case "left":
+                self.left = value
+            case "height":
+                self.height = value
+            case "rotation":
+                self.rotation = value
+            case "fill_fore_color":
+                self.fill.solid()
+                self.fill.fore_color.rgb = value
+            case "line_color":
+                self.line.color.rgb = value
+            case "line_width":
+                self.line.width = value
+
+    def randomize(self):
+        random_properties = {
+            # "shape_type": random.choices(
+            #     [member.value for member in MSO_SHAPE.__members__], 
+            #     [5 if member.name == "RECTANGLE" else 1 for member in MSO_SHAPE.__members__]
+            # )[0], 
+            "line_type": random.choices(
+                [member.value for member in MSO_LINE.__members__ if member.name != "DASH_STYLE_MIXED"], 
+                [5 if member.name == "SOLID" else 1 for member in [member for member in MSO_LINE.__members__ if member.name != "DASH_STYLE_MIXED"]]
+            )[0],
+            "left": Inches(random.uniform(0, 8)), 
+            "height": Inches(random.uniform(0, 6)),  
+            "rotation": random.randint(0, 360),  # k_modal_distribution([0, 90, 180, 270], [3, 1, 3, 1]), 
+            "fill_fore_color": random_rgb_color(), 
+            "line_color": random_rgb_color(), 
+            "line_width": Pt(random.randint(0, 5)),
+        }
+        for property, value in random_properties.items():
+            self.set(property, value)
+        return random_properties
+
+    setattr(cls, "set", set)
+    setattr(cls, "randomize", randomize)
+
+    return cls
+
+add_custom_manipulator(Shape)
 
 class PPT:
     def __init__(self):
@@ -22,39 +79,22 @@ class PPT:
 
         self.shapes = dict((member.name, member.value) for member in MSO_SHAPE.__members__)
 
-        self.lines = dict((member.name, member.value) for member in MSO_LINE.__members__ if member.name != "DASH_STYLE_MIXED") # DASH_STYLE_MIXED is not supported by python-pptx
-
-        shape_max_size = (Inches(4), Inches(4))
-        self.base_shape_visual_property_range = {
-            "left": (0, self.prs.slide_width - shape_max_size[0]), 
-            "top": (0, self.prs.slide_height - shape_max_size[1]), 
-            "width": (0, shape_max_size[0]), 
-            "height": (0, shape_max_size[1]), 
-            "rotation": (0, 360), 
-        }
-    
     def add_blank_slide(self):
         self.prs.slides.add_slide(self.prs.slide_layouts.get_by_name("Blank"))
         
     def generate_random_shape(self):
-        shapes = random.choice(list(self.shapes.items()))
+        shape_type = random.choice(list(self.shapes.items()))
         self.prs.slides[-1].shapes.add_shape(
-            shapes[1], 
+            shape_type[1], 
             Inches(1), Inches(1), Inches(1),  Inches(1)
         )
 
         shape = self.prs.slides[-1].shapes[-1]
 
-        for property, value_range in self.base_shape_visual_property_range.items():
-            setattr(shape, property, random.randint(*value_range))
-
-        shape.fill.solid()
-        shape.fill.fore_color.rgb = self._generate_random_rgb_color()
-        shape.line.color.rgb = self._generate_random_rgb_color()
-        shape.line.dash_style = random.choice(list(self.lines.values()))
-        shape.line.width = Inches(random.randrange(0, 1))
+        property = shape.randomize()
+        property["shape_type"] = shape_type[0]
         
-        return shape
+        return property
     
     def _generate_random_rgb_color(self):
         return RGBColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
@@ -62,9 +102,16 @@ class PPT:
     def save(self):
         self.prs.save('ppt_object_dataset.pptx')
 
+def synthesize_data(slides=10, shape_per_slide=4):
+    ppt = PPT()
+    answer = []
+    for slide in range(slides):
+        ppt.add_blank_slide()
+        for shape in range(shape_per_slide):
+            answer.append(ppt.generate_random_shape())
+    ppt.save()
+    with open('answer.json', 'w') as f:
+        json.dump(answer, f, indent=4)
 
-ppt = PPT()
-ppt.add_blank_slide()
-for i in range(5):
-    ppt.generate_random_shape()
-ppt.save()
+if __name__ == '__main__':
+    synthesize_data()
